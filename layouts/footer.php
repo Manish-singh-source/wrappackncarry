c5b8de485a03dcd68dbf1cf5bb1691ea397881ab
Roshan <support@technofra.com>
2026-04-29 17:23:49 +0530

    footer updated

--- layouts/footer.php
+++ layouts/footer.php
@@ -95,10 +95,10 @@
                         <h2 class="widget-title">Our Product</h2>
                         <div class="textwidget">
                             <ul>
-                                <li><a href="#">Oil Grease Resistant Paper</a></li>
-                                <li><a href="#">Baking & Cooking Paper</a></li>
-                                <li><a href="#">Slip Easy Paper</a></li>
-                                <li><a href="#">Sachet Paper</a></li>
+                                <li><a href="oil-grease-resistant-paper.php">Oil Grease Resistant Paper</a></li>
+                                <li><a href="baking-and-cooking-paper.php">Baking & Cooking Paper</a></li>
+                                <li><a href="kraft-paper-bag.php">Kraft Paper Bag</a></li>
+                                <li><a href="tableware.php">Tableware</a></li>
                             </ul>
                         </div>
                     </div>
@@ -108,10 +108,10 @@
                         <h2 class="widget-title">&nbsp;</h2>
                         <div class="textwidget">
                             <ul>
-                                <li><a href="#">Kraft Paper Bag</a></li>
-                                <li><a href="#">MG Poster Bag</a></li>
-                                <li><a href="#">Pizza Boxes</a></li>
-                                <li><a href="#">Tableware</a></li>
+                                <li><a href="pizza-boxes.php">Pizza Boxes</a></li>
+                                <li><a href="slip-easy-paper.php">Slip Easy Paper</a></li>
+                                <li><a href="sachet-paper.php">Sachet Paper</a></li>
+                                <li><a href="mg-poster-bag.php">MG Poster Bag</a></li> 
                             </ul>
                         </div>
                         <!-- <div class="pbmit-timelist-wrapper">
